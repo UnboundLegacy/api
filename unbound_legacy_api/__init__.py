5df5fa925e2a7f2373c60963b73fe94e9f32a809
Justin Siebert <Just.Sieb@gmail.com>
2017-10-31 21:12:13 -0400

    feat(user,auth): Add in basic create account and authentication

--- unbound_legacy_api/__init__.py
+++ unbound_legacy_api/__init__.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, g
 
-from .blueprints import stats_bp
+from .blueprints import stats_bp, user_bp
+from .database import init_app_db
 
 def create_app(config=None, environment=None):
     app = Flask(__name__)
+    app.config.from_object('instance.base.Config')
 
-    for blueprint in [stats_bp]:
+    init_app_db(app)
+    configure_hook(app)
+
+    for blueprint in [stats_bp, user_bp]:
         app.register_blueprint(blueprint)
 
     return app
+
+def configure_hook(app):
+    @app.before_request
+    def create_session():
+        g.db = app.config['SESSION']()
+
+    @app.teardown_request
+    def teardown_request(exception):
+        app.config['SESSION'].remove()
