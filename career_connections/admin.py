580536e485604771f3588827d228d4a78ee91980
Justin Siebert <just.sieb@gmail.com>
2018-01-06 22:06:46 -0500

    fix(admin): Update admin model view titles

--- career_connections/admin.py
+++ career_connections/admin.py
@@ -6,5 +6,5 @@ from career_connections.database import models
 def configure_admin(app, db_session):
     '''Configure Admin pages for Career Connections'''
     admin = Admin(app, name='Unbound Legacy', template_mode='bootstrap3')
-    admin.add_view(ModelView(models.User, db_session, name='user_admin'))
-    admin.add_view(ModelView(models.PendingUser, db_session, name='pending_user_admin'))
+    admin.add_view(ModelView(models.User, db_session, name='Users'))
+    admin.add_view(ModelView(models.PendingUser, db_session, name='Pending Users'))
